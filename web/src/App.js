5be44c5fffd20e2967f84d23eed747bcd01ba826
ak <kochkin.andrey@gmail.com>
2021-02-24 16:08:04 +0000

    react client update

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,13 @@
 import React, { useEffect } from "react";
 import './App.css';
 
+import iconThrowIn from './svg/throw-in.svg'
+import iconFreeKick from './svg/free-kick.svg'
+import iconGoalKick from './svg/goal-kick.svg'
+import iconCornerKick from './svg/corner-kick.svg'
+import iconOffside from './svg/offside.svg'
+import iconGoal from './svg/goal.svg'
+
 const Loader = function() {
   return <div className="Loader">⚽</div>
 }
@@ -18,6 +25,8 @@ function App() {
   const [matchItem, setMatchItem] = React.useState(null)
 
   React.useEffect(() => {
+    setMatchItem(null)
+
     fetch('/api/match/' + selectedItem)
       .then((res) => res.json())
       .then((data) => setMatchItem(data))
@@ -25,18 +34,22 @@ function App() {
 
   return (
     <div className="App">
-      <header></header>
+      <header className="Header">
+        {/* <h1>Header</h1> */}
+      </header>
 
-      <main>
-        <aside style={{width: '45%', margin: 0, float: 'left'}}>
+      <main className="Content">
+        <aside style={{width: '50%', margin: 0}}>
           <MatchList matchList={matchList} selectMatchItem={selectMatchItem}/>
         </aside>
-        <aside style={{width: '45%', margin: 0, float: 'right'}}>
+        <aside style={{width: '50%', margin: 0}}>
           <MatchItem matchItem={matchItem} />
         </aside>
       </main>
 
-      <footer></footer>
+      <footer className="Footer">
+        {/* <h2>Footer</h2> */}
+      </footer>
     </div>
   );
 }
@@ -70,6 +83,7 @@ const MatchList = function ({matchList, selectMatchItem}) {
               <span className="MatchLegion">🏆&nbsp;{item.competition_name}</span>
               <span className="MatchRegion">🌍&nbsp;{item.region_name}</span>
             </p>
+            <p>{item.updates ? '+' : '-'}</p>
           </li>
         })}
       </ul>
@@ -94,11 +108,40 @@ const MatchItem = function ({matchItem}) {
       })
 
       if(matchItem.updates) {
-        setEvents(matchItem.updates.filter(item => item.events != undefined))
-        setStates(matchItem.updates.filter(item => item.state != undefined))
-      }
+        // setEvents(matchItem.updates.filter(item => item.events != undefined))
+        // setStates(matchItem.updates.filter(item => item.state != undefined))
 
-      console.log(matchItem)
+        let currentState = {
+          states: [],
+          events: {}
+        }
+
+        matchItem.updates.forEach(item => {
+          if(item.betstop) {
+            currentState.betstop = true
+            console.log('betting stopped')
+          }
+
+          if(item.state) {
+            currentState.states.push(item.state)
+            // console.log(item.state)
+          }
+
+          if(item.events) {
+            currentState.events = Object.assign(currentState.events, item.events)
+            // console.log(item.events)
+          }
+
+          if(item.markets) {
+            console.log('markets changed', item.markets)
+          }
+        })
+
+        setEvents(currentState.events)
+        setStates(currentState.states)
+
+        console.log(currentState)
+      }
     }
   }, [matchItem])
 
@@ -107,31 +150,69 @@ const MatchItem = function ({matchItem}) {
       <div>
         <h2>{ !teams ? <Loader /> : `${teams.home.name} ⚽ ${teams.away.name}`}</h2>
         <div className="MatchInfo">
-          { !states ? '' : states.map((item, index) => {
-            return <div key={index}>
-              {item.state.type} 
-              ⏰{!item.state.server_time ? 'unknown' : new Date(item.state.server_time).toLocaleString()}
-            </div>
-          })}
 
-          {!events ? '' : events.map((item, index) => {
-            return <div key={index}>
-              {
-                Object.values(item.events)
-                  .sort((e1, e2) => { return e1.match_time > e2.match_time })
-                  .map((item, index) => {
-                    return <p key={index} className={item.type}>
-                      {`${Math.trunc(item.match_time / 60)}:${(item.match_time % 60).toString().padStart(2, 0)}' `}
-                      {!item.additional_time ? '' : `${Math.trunc(item.additional_time / 60)}:${(item.additional_time % 60).toString().padStart(2, 0)}' `}
-                      {item.name} [{item.side}] {item.ball_pos ? `${item.ball_pos.x}, ${item.ball_pos.y}` : '()'}
-                    </p>
-                  })
-              }
+          {events && (
+            <div>
+              <span>
+                <img src={iconThrowIn} />
+                <span>{Object.values(events).filter(item => item.type == 'throw-in').length}</span>
+              </span>
+              <span>
+                <img src={iconFreeKick} />
+                {Object.values(events).filter(item => item.type == 'free-kick').length}
+              </span>
+              <span>
+                <img src={iconGoalKick} />
+                {Object.values(events).filter(item => item.type == 'goal-kick').length}
+              </span>
+              <span>
+                <img src={iconCornerKick} />
+                {Object.values(events).filter(item => item.type == 'corner-kick').length}
+              </span>
+              <span>
+                <img src={iconOffside} />
+                {Object.values(events).filter(item => item.type == 'offside').length}
+              </span>
+              <span>
+                <img src={iconGoal} />
+                {Object.values(events).filter(item => item.type == 'goal').length}
+              </span>
             </div>
-          })}
+          )}
+
+          {states && (
+            <div>
+              {states.map((item, index) => {
+                return <div key={index}>
+                  {item.type}&nbsp;⏰&nbsp;{!item.server_time ? 'unknown' : new Date(item.server_time).toLocaleString()}
+                </div>
+              })}
+            </div>
+          )}
+
+          
+          {events && (
+            <div className="EventList">
+              {Object.values(events).map((item, index) => {
+                return <div key={index} className={`${item.type} ${item.side}`}>
+                  {`${Math.trunc(item.match_time / 60)}:${(item.match_time % 60).toString().padStart(2, 0)}' `}
+                  {!item.additional_time ? '' : `${Math.trunc(item.additional_time / 60)}:${(item.additional_time % 60).toString().padStart(2, 0)}' `}
+                  {item.name} [{item.side}] {item.ball_pos ? `(${item.ball_pos.x}, ${item.ball_pos.y})` : '()'}
+                </div>
+              })}
+            </div>
+          )}
         </div>
       </div>
     </div>
+  )
+}
+
+const MatchItemUpdates = function({item}) {
+  return (
+    item.map((item) => {
+
+    })
   )
 }
 
