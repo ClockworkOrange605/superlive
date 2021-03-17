b2574dabe6f2cc110655f3fd7e1cc12ebef0e8c4
ak <kochkin.andrey@gmail.com>
2021-03-17 10:53:08 +0000

    better script

---
+++ better.js
@@ -0,0 +1,226 @@
+const config = require('./config/main.js')
+
+const ApiClient = require('./src/client_v2.js')
+
+setInterval(function() {
+  getMatches({offset: 0, limit: 5}, (matches) => {
+    matches.filter(item => {
+      return item.start > Date.now() && 
+      item.start < (Date.now() + 300000)
+    }).forEach(match => processMatch(match))
+  })
+}, 300000)
+
+function getMatches({limit=10, offset=0}, callback) {
+  new ApiClient(config.betboom, (client) => {
+    client.emmiter.on('socket.opened', () => {
+      client.authorize(() => {
+        client.getMatchlist({offset: offset, limit: limit}, (response) => {
+          client.socket.close()
+
+          const result = response.matches
+            .map(item => ({
+              _id: undefined,
+              id: item.id,
+              sport_id: item.sport_id,
+              region_id: undefined,
+              competition_id: undefined,
+              sport_name: item.sport_name,
+              region_name: item.region_name,
+              competition_name: item.competition_name,
+              teams: {
+                home: {
+                  id: Object.keys(item.teams).find(key => item.teams[key].side == 'home'),
+                  name: Object.values(item.teams).find(item => item.side == 'home').name.trim()
+                },
+                away: {
+                  id: Object.keys(item.teams).find(key => item.teams[key].side == 'away'),
+                  name: Object.values(item.teams).find(item => item.side == 'away').name.trim()
+                }
+              },
+              start: item.start_time,
+              state: item.state.type,
+              scores: {
+                home: item.scores.total.home,
+                away: item.scores.total.away
+              }
+            }))             
+          
+          callback(result)
+        })
+      })  
+    })
+  })
+}
+
+function processMatch(match) {
+  new ApiClient(config.betboom, (client) => {
+    client.emmiter.on('socket.opened', () => {
+      client.authorize(() => {          
+        client.subscribeMatch(match, (response) => {
+          console.log('Subscribed', response[match.id].teams)
+          let matchState = {...match, ...response[match.id], ...{updates: [], events: [], mainEvents: [], betting: {better1: false, better2: false, bets: []}, currentMarkets: {}} }
+
+          client.emmiter.on('api.match.update', (message) => {
+            matchState.updates.push({...message[matchState.id], recieved_at: Date.now()})
+
+            if(matchState.betstop != message[matchState.id].betstop) {
+              console.log('Betting', message[matchState.id].betstop ? 'disabled' : 'enabled', Date.now())
+              matchState.betstop = message[matchState.id].betstop
+            }
+
+            /**/if(Object.keys(message[matchState.id]).find(key => !['start_time', 'state', 'scores', 'markets', 'betstop', 'events'].includes(key)) != undefined) {
+              console.warn('Update not processed ', Object.keys(message[matchState.id]).find(key => !['start_time', 'state', 'scores', 'markets', 'betstop', 'events'].includes(key)))
+            }/**/
+          })
+
+          client.emmiter.on('api.match.update.markets', (markets) => {
+            matchState.currentMarkets = markets
+          })
+
+          client.emmiter.on('api.match.update.events', (events) => {
+            let matchEvents = Object.keys(events)
+              .map(key => ({...{id: key}, ...events[key]}))
+              .sort((el1, el2) => el1.server_time - el2.server_time)
+
+            matchState.events.push(...matchEvents)
+
+            let mainEvent = matchEvents
+              .find(item =>
+                ['throw-in', 'free-kick', 'goal-kick', 'goal', 'corner-kick', 'offside']
+                .includes(item.type))
+
+            if(mainEvent != undefined) {
+              console.log(mainEvent.type)
+
+              if(matchState.betting.better2 && mainEvent.type == 'free-kick') {
+                console.log('bettor2', '!throw-in')
+
+                let market = {}
+                market.type = '!throw-in'
+                market.id = Object.keys(matchState.markets).find(key => matchState.markets[key].type == market.type)
+                market.price = matchState.currentMarkets[market.id] != undefined ? matchState.currentMarkets[market.id].price : matchState.markets[market.id].price
+
+                console.log('Make Bet', {id: matchState.id}, market, 2000)
+
+                client.makeBet({id: matchState.id}, market, 2000, (response) => {
+                  console.log('Bet Response', response)
+                  matchState.betting.bets.push({...response, type: 'bettor2'})
+                })
+
+              }
+
+              if(matchState.betting.better1 && mainEvent.type == 'throw-in' && matchState.mainEvents[matchState.mainEvents.length - 1].type == 'throw-in') {
+                console.log('bettor1', '!throw-in')
+
+                let market = {}
+                market.type = '!throw-in'
+                market.id = Object.keys(matchState.markets).find(key => matchState.markets[key].type == market.type)
+                market.price = matchState.currentMarkets[market.id] != undefined ? matchState.currentMarkets[market.id].price : matchState.markets[market.id].price
+
+                console.log('Make Bet', {id: matchState.id}, market, 2000)
+
+                client.makeBet({id: matchState.id}, market, 2000, (response) => {
+                  console.log('Bet Response', response)
+
+                  console.log({...response, type: 'bettor1'})
+                  matchState.betting.bets.push({...response, type: 'bettor1'})
+                })
+              }
+
+              matchState.mainEvents.push(mainEvent)
+            }
+          })
+
+          client.emmiter.on('api.bet.update', (message) => {
+            console.log('Bet Update', message)
+            console.log('Bet in pool',
+             Object.keys(matchState.betting.bets).find(key => 
+              matchState.betting.bets[key].bet_id == message.bet_id)
+            )
+            console.log('Bets List', matchState.betting.bets)
+          })
+
+          client.emmiter.on('api.match.update.state', (state) => {
+            console.log('Update State', state)
+            matchState.state = state
+
+            // check stats
+            if(state.type == 'halftime') {
+              let stats1 = {}
+              matchState.mainEvents.forEach((item, index, arr) => {
+                if(index >= 2 && index < arr.length - 1) {
+                  let key = arr[index-2].type+'->'+arr[index-1].type+'->'+item.type
+                  if(stats1[key] == undefined) stats1[key] = []
+                  stats1[key].push(arr[index+1].type)
+                }
+              })
+
+              let stats2 = {}
+              matchState.mainEvents.forEach((item, index, arr) => {
+                if(index >= 1 && index < arr.length - 1) {
+                  let key = arr[index-1].type+'->'+item.type
+
+                  if(stats2[key] == undefined) stats2[key] = []
+                  stats2[key].push(arr[index+1].type)
+                }
+              })
+
+              let filtered1 = Object.keys(stats1)
+                .sort()
+                .filter(key => key.startsWith('throw-in->throw-in')) // < 20% !throw-in
+
+              let filtered2 = Object.keys(stats2)
+                .sort()
+                .filter(key => key.startsWith('free-kick')) // < 15% !throw-in
+
+              let count1 = filtered1.reduce((sum, key) => sum + stats1[key].length, 0)
+              let count2 = filtered2.reduce((sum, key) => sum + stats2[key].length, 0)
+
+              if(stats1['throw-in->throw-in->throw-in'] != undefined) {
+                console.log('Statistics1 %', (stats1['throw-in->throw-in->throw-in'].length / count1 * 100).toFixed(1), 20)
+              }
+
+              if(stats2['free-kick->throw-in'] != undefined) {
+                console.log('Statistics2 %', (stats2['free-kick->throw-in'].length / count2 * 100).toFixed(1), 30)
+              }
+
+              if(stats1['throw-in->throw-in->throw-in'] == undefined ||
+                (stats1['throw-in->throw-in->throw-in'].length / count1 * 100).toFixed(1) < 20) {
+                  console.log('Betting 1', true)
+                  matchState.betting.better1 = true
+              }
+
+              if(stats2['free-kick->throw-in'] == undefined ||
+                (stats2['free-kick->throw-in'].length / count2 * 100).toFixed(1) < 30) {
+                  console.log('Betting 2', true)
+                  matchState.betting.better2 = true
+              }
+            }
+          })
+
+          client.emmiter.on('api.match.update.scores', (scores) => {
+            console.log('Update Scores', scores)
+            matchState.scores = scores
+          })
+
+          client.emmiter.on('api.match.unsubscribed', (message) => {
+            console.log('Unsibscribed', message)
+
+            console.log(matchState)
+            client.socket.close()
+          })
+        })
+        
+      })
+    })
+  
+    client.emmiter.on('socket.closed', (status) => {
+      console.log('Socket Closed', status)
+    })
+  
+    client.emmiter.on('socket.error', (error) => {
+      console.error('Socket Error', error)
+    })
+  })
+}
---
+++ src/client_v2.js
@@ -0,0 +1,164 @@
+const atob = require('atob');
+const { v4: uuidv4 } = require('uuid');
+
+const EventEmitter = require('events');
+const WebSocket = require('ws');
+
+class ApiClient {
+
+  constructor({url, token}, callback) {
+    this.token = token
+
+    this.emmiter = new EventEmitter()
+    this.socket = new WebSocket(url)
+
+    this.socket.on('open', () => {
+      this.emmiter.emit('socket.opened', {})
+    })
+
+    this.socket.on('close', (code) => {
+      this.emmiter.emit('socket.closed', {statusCode: code})
+    })
+
+    this.socket.on('error', (error) => {
+      this.emmiter.emit('socket.error', {error: error})
+    })
+
+    this.socket.on('message', (data) => {
+      let message = JSON.parse(data)
+
+      this.emmiter.emit('socket.data.recieved', message)
+
+      if(message.error) {
+        this.emmiter.emit('socket.data.error', {
+          type: message.type,
+          error: {
+            code: message.error.code,
+            message: message.error.text
+          }          
+        })
+
+        throw('WS API ERROR', {type: message.type, error: message.error.text})
+      }
+
+      switch(message.type) {
+        case 'session-info':
+          this.emmiter.emit('api.session', message.payload)
+        break
+
+        case 'auth':
+          this.emmiter.emit('api.auth', message.payload)
+        break
+
+        case 'list-matches-v2':
+          this.emmiter.emit('api.matchlist', message.payload)
+        break
+
+        case 'subscribe-match':
+          this.emmiter.emit('api.match.subscribed', message.payload)
+        break
+
+        case 'autounsubscribe-match':
+          this.emmiter.emit('api.match.unsubscribed', message.payload)
+        break
+
+        case 'match-update':
+          this.emmiter.emit('api.match.update', message.payload)
+        break
+
+        case 'make-bet':
+          this.emmiter.emit('api.bet.new', message.payload)
+        break
+
+        case 'bet-update-v2':
+          this.emmiter.emit('api.bet.update', message.payload)
+        break
+
+        case 'balance-changed':
+          this.emmiter.emit('api.user.balance', message.payload)
+        break
+
+        default:
+          console.warn('Unprocessed Message', message.type, message.payload)
+        break
+      }
+    })
+
+    callback(this)
+  }
+
+  authorize(callback) {
+    let payload = JSON.parse(atob(this.token))
+    let request = prepareRequest('auth', payload)
+
+    this.socket.send(request)
+    this.emmiter.once('api.auth', (response) => callback(response))
+  }
+
+  getMatchlist({state="all", offset=0, limit=10}, callback) {
+    let payload = {
+      filters: { match_state: state, sport_ids: ["soccer"] },
+      offset: offset, limit: limit
+    }
+    let request = prepareRequest('list-matches-v2', payload)
+
+    this.socket.send(request)
+    this.emmiter.once('api.matchlist', (response) => callback(response))
+  }
+
+  subscribeMatch({id}, callback) {
+    let request = prepareRequest('subscribe-match', {id: id})
+
+    this.emmiter.on('api.match.update', (response) => {
+      if(response[id] != undefined) {
+
+        if(response[id].markets != undefined) {
+          this.emmiter.emit('api.match.update.markets', response[id].markets)
+        }
+
+        if(response[id].events != undefined) {
+          this.emmiter.emit('api.match.update.events', response[id].events)
+        }
+
+        if(response[id].state != undefined) {
+          this.emmiter.emit('api.match.update.state', response[id].state)
+        }
+
+        if(response[id].scores != undefined) {
+          this.emmiter.emit('api.match.update.scores', response[id].scores)
+        }
+      }
+    })
+
+    this.socket.send(request)
+    this.emmiter.once('api.match.subscribed', (response) => callback(response))
+  }
+
+  makeBet(match, market, amount, callback) {
+    let request = prepareRequest('make-bet', {
+      match_id: match.id,
+      market: {
+        id: market.id,
+        type: market.type,
+        price: market.price,
+      },
+      amount: amount,
+      allow_price_change: true,
+      is_auto_bet: false,
+      is_max_bet: false,
+    })
+
+    this.socket.send(request)
+    this.emmiter.once('api.bet.new', (response) => callback(response))
+  }
+}
+
+function prepareRequest(type, payload) {
+  return JSON.stringify({
+    type: type,
+    payload: payload,
+    rid: uuidv4()
+  })
+}
+
+module.exports = ApiClient
